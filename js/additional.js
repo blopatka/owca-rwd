94664acba6db573e947190cccef70c500be48bcf
blopatka <bartlomiej.lopatka@gmail.com>
2019-03-14 19:45:05 +0100

    polish script

--- js/additional.js
+++ js/additional.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-  $('a').click(function(e) {
-    if(isInternalLink(this) && redirectTresholdHit()) {
-      redirectToPrimaAprilis(this);
-    }
-    e.preventDefault();
-  });
+    $('a').click(function(e) {
+        if(isInternalLink(this) && redirectTresholdHit()) {
+            redirectToPrimaAprilis(this);
+            e.preventDefault();
+        }
+    });
 });
 
 function isInternalLink(element) {
-  return $(element).attr("href").startsWith("view");
+    return $(element).attr("href").startsWith("view");
 }
 
 function redirectTresholdHit() {
-  return 50 > Math.floor(Math.random() * 100);
+    return 5 > Math.floor(Math.random() * 100);
 }
 
 function redirectToPrimaAprilis(c) {
-  $(c).hide();
-  // window.location = 'http://www.pentax.pl/500.html';
+    window.location = 'http://www.pentax.pl/index503.html';
 }
