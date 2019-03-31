bd31ca3fa7715c39656085f895eb5a098ff83a3e
blopatka <bartlomiej.lopatka@gmail.com>
2019-03-31 21:42:06 +0200

    up aprilis

--- js/additional.js
+++ js/additional.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
     $('a').click(function(e) {
-        if(isInternalLink(this) && redirectTresholdHit()) {
+        if(isPrimaAprilis() && isInternalLink(this) && redirectTresholdHit()) {
             redirectToPrimaAprilis();
             e.preventDefault();
         }
     });
 });
 
+function isPrimaAprilis() {
+	var d = new Date();
+	return 1 === d.getDate() && 3 === d.getMonth();
+}
+
 function isInternalLink(element) {
     return ($(element).attr("href").indexOf("viewtopic") !== -1) || ($(element).attr("href").indexOf("viewforum") !== -1);
 }
 
 function redirectTresholdHit() {
-    return 5 > Math.floor(Math.random() * 100);
+    return 10 > Math.floor(Math.random() * 100);
 }
 
 function redirectToPrimaAprilis() {
-    window.location = 'http://www.pentax.org.pl/index503.html';
+    window.location = 'http://www.pentax.org.pl/index-503.html';
 }
